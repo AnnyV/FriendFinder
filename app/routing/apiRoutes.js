987df54fd34b4aff8873294e617ce90e8241c947
Annyv <annybella18@gmail.com>
2017-02-26 17:15:20 -0500

    best match working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,4 @@
-
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var peopleData = require("../data/friends.js");
 
 
 // ===============================================================================
@@ -15,30 +13,54 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(tableData);
+    res.json(peopleData);
   });
 
-  
+
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // Then the server saves the data to the peopleData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
+    var user = req.body;
+    var bestMatch;
+    var leastDiff;
+    var currentDiff;
+
+    for (var i = 0; i < peopleData.length; i++) {
+      currentDiff = 0;
+      for (var j = 0; j < peopleData[i].scores.length; j++) {
+        currentDiff += Math.abs(parseInt(user.scores[j]) - parseInt(peopleData[i].scores[j]));
+
+      }
+      console.log("i " + i);
+      console.log("currentDiff " + currentDiff);
+      console.log("leastDiff " + leastDiff);
+      console.log("-----------------------");
+      if (i === 0) {
+        bestMatch = 0;
+        leastDiff = currentDiff;
+      } else {
+        if (currentDiff < leastDiff) {
+          bestMatch = i;
+          leastDiff = currentDiff;
+        }
+      }
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+
+    peopleData.push(user);
+
+    res.send(peopleData[bestMatch]);
+    //display best match///////
+    console.log("best Match is " +peopleData[bestMatch].name);
+
   });
 
   // ---------------------------------------------------------------------------
@@ -47,9 +69,9 @@ module.exports = function(app) {
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    peopleData = [];
 
-    console.log(tableData);
+
+    console.log(peopleData);
   });
 };
